c2ea8e09561f66ba5d93f60801c8a4d08c8eeacd
lozlof <contact@gistyr.dev>
2025-11-13 16:48:24 -0500

    Add mod.rs

---
+++ src/routes/mod.rs
@@ -0,0 +1,16 @@
+//!/////// START OF FILE //////////
+//! GOBSG
+//!
+//! Copyright (c) 2025 Gistyr LLC
+//!
+//! Licensed under the PolyForm Small Business License 1.0.0
+//! See LICENSES/LICENSE-POLYFORM-SMALL-BUSINESS.md or https://polyformproject.org/licenses/small-business/1.0.0
+//!
+//! Required Notice: Copyright Gistyr LLC (https://gistyr.dev)
+//!
+//! For full licenses see:
+//! LICENSES/
+//!
+//! ---------------------------------------- //
+
+////////// END OF FILE //////////
