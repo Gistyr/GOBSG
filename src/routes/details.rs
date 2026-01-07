106a5b6bbe936ce06384299a90c0f4bc0f1edabb
Lozlof <contact@gistyr.dev>
2026-01-07 10:22:56 -0500

    Update banners to 2026

--- src/routes/details.rs
+++ src/routes/details.rs
@@ -1,7 +1,7 @@
 //!/////// START OF FILE //////////
 //! GOBSG
 //!
-//! Copyright (c) 2025 Gistyr LLC
+//! Copyright (c) 2026 Gistyr LLC
 //!
 //! Licensed under the PolyForm Small Business License 1.0.0
 //! See LICENSES/LICENSE-POLYFORM-SMALL-BUSINESS.md or https://polyformproject.org/licenses/small-business/1.0.0
